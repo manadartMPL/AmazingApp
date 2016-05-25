f0c5a9d1f0f54a91332e1c07df5402f906833691
Michael <mwoo727@gmail.com>
2016-05-25 15:55:26 -0700

    Fixed problem with reading null array. Added comments including javadoc comments.

--- app/src/main/java/mwoo/amazingapp/MazeView.java
+++ app/src/main/java/mwoo/amazingapp/MazeView.java
@@ -10,32 +10,70 @@ import android.view.View;
 /**
  * Created by Michael on 5/23/2016.
  */
-public class MazeView extends View{
+public class MazeView extends View {
+    /**
+     * Got this from code online when searching on how build maze. Not sure yet what to do with it. May delete later on.
+     * But its supposed to help when drawing the walls of the maze.
+     */
     private static final float wallsize = 5;
+
+    /**
+     * paint is needed so that app knows how to draw the walls, trails, and characters.
+     */
     private Paint paint;
+
+    /**
+     * Width of the screen that the view is on
+     */
     private int width;
+
+    /**
+     * Height of the screen that the view is on
+     */
     private int height;
+
+    /**
+     * Holds the characters that dictate where the player and their trail is in the maze and the walls.
+     */
     private char[][] maze;
 
-    public MazeView(Context context, AttributeSet attset){
-        super(context,attset);
-
+    /**
+     * Constructor of MazeView
+     * @param context not sure right now
+     * @param attset Not sure right now
+     */
+    public MazeView(Context context, AttributeSet attset) {
+        super(context, attset);
         paint = new Paint();
         paint.setAntiAlias(true);
-        int width = getWidth();
-        int height = getHeight();
-        maze = GetMaze.getMazeLayout();
     }
 
+    /**
+     * Gets the measurements of the screen. This method is repeatedly called while MazeView is active
+     * @param widthMeasureSpec Not sure right now
+     * @param heightMeasureSpec Not sure right now
+     */
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        height = MeasureSpec.getSize(heightMeasureSpec)/GetMaze.getRows();      //gets the height of the screen which the view is on
+        width = MeasureSpec.getSize(widthMeasureSpec)/GetMaze.getCols();        //same as above but for width
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    /**
+     * Draws the maze on the screen and will hopefully draw the game.
+     * But need to adjust measurements
+     * @param canvas Where the maze is being drawn on.
+     */
     @Override
-    public void onDraw(Canvas canvas){
-        for(int col = 0;col < GetMaze.getCols();col++){
-            for(int row = 0; row < GetMaze.getRows();row++){
-                if(maze[row][col] == '*'){
-                    paint.setColor(Color.BLUE);
-                    paint.setStyle(Paint.Style.FILL);
-                    canvas.drawRect(col*width,row*height, width,height,paint);
-                }
+    public void onDraw(Canvas canvas) {
+        maze = GetMaze.getMazeLayout();                                             //Gets the chars in the maze to create it
+        for (int col = 0; col < GetMaze.getCols(); col++) {
+            for (int row = 0; row < GetMaze.getRows(); row++) {
+                if (maze[row][col] == '*') {                                        //If the space in maze has the * character then draw a blue rectangle to depict wall
+                    paint.setColor(Color.BLUE);                                     //Set color of rectangle to blue
+                    paint.setStyle(Paint.Style.FILL);                               //Fill in rectangle
+                    canvas.drawRect(col*width, row*height, width, height, paint);   //Draw rectangle at the coordinates
+                }                                                                   //Format is (left point, top point, right point, bottom point, how to draw it)
             }
         }
     }
